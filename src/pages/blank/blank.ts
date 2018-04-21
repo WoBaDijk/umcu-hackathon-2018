3d643d8ca6414c4f06b82c565aafb54b410b89f5
WoBaDijk <wouter@van-dijk.nu>
2018-04-21 11:43:50 +0200

    Added images

---
+++ src/pages/blank/blank.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+@Component({
+    selector: 'page-blank',
+    templateUrl: 'blank.html'
+})
+export class BlankPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
--- src/pages/welcome/welcome.ts
+++ src/pages/welcome/welcome.ts
@@ -5,11 +5,11 @@ import {Component} from '@angular/core';
     templateUrl: 'welcome.html'
 })
 export class WelcomePage {
-    
-    // openApp() {
-    //     // Reset the content nav to have just this page
-    //     // we wouldn't want the back button to show in this scenario
-    //     this.nav.setRoot(Blank);
-    // }
+    constructor(public navCtrl: NavController) {}    
+    openApp() {
+        // Reset the content nav to have just this page
+        // we wouldn't want the back button to show in this scenario
+        this.nav.setRoot(Blank);
+    }
 
 }
