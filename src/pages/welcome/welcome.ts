ecdfd7f0fb5ecf0eb43fd4cb438f42789d80f5d8
WoBaDijk <wouter@van-dijk.nu>
2018-04-21 11:50:24 +0200

    Fix bugs welcome screen

--- src/pages/welcome/welcome.ts
+++ src/pages/welcome/welcome.ts
@@ -1,15 +1,18 @@
 import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+import {BlankPage} from '../blank/blank';
 
 @Component({
     selector: 'page-welcome',
     templateUrl: 'welcome.html'
 })
 export class WelcomePage {
-    constructor(public navCtrl: NavController) {}    
+    constructor(public navCtrl: NavController) {}
+    
     openApp() {
         // Reset the content nav to have just this page
         // we wouldn't want the back button to show in this scenario
-        this.nav.setRoot(Blank);
+        this.navCtrl.setRoot(BlankPage);
     }
 
 }
