96cc6764ba2a674eb660584343a428f4a5b4c514
TimPijl <tim.pijl@finaps.nl>
2018-04-21 22:21:41 +0200

    Graph thingy

--- src/pages/detailpage/detailpage.ts
+++ src/pages/detailpage/detailpage.ts
@@ -1,15 +1,74 @@
 import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
+import * as HighCharts from 'highcharts';
 
 @Component({
-    selector: 'page-detail',
+    selector: 'page-blank',
     templateUrl: 'detailpage.html'
 })
 export class DetailPage {
 
   constructor(public navCtrl: NavController) {
-
   }
 
+// > 150 heartrate == rood
+  ionViewDidLoad() {
+    var myChart = HighCharts.chart('container', {
+      chart: {
+        type: 'spline'
+      },
+      title: {
+        text: 'Heartrate'
+      },
+      xAxis: {
+        categories: ['Eergisteren', 'Gisteren', 'Vandaag', 'Morgen']
+      },
+      yAxis: {
+        title: {
+          text: 'Hartslag'
+        },
+        plotLines: [{
+          value: 140,
+          color: 'red',
+          dashStyle: 'shortdash',
+          width: 2,
+          label: {
+            text: 'Bovengrens hartslag'
+          }
+        }]
+      },
+      series: [
+        {
+          name: 'YOU',
+          data: [160, 155, 145],
+          lineWidth: 10,
+          zones: [{
+            value: 140,
+            color: '#009345',
+            }, {
+            value: 150,
+            color: '#EC1C24' // oranje
+            }, {
+            color: '#EC1C24',
+            }]
+        },
+        {
+          name: 'PREDICT',
+          data: [null, null, 145, 130],
+          lineWidth: 10,
+          dashStyle: 'dash',
+          zones: [{
+            value: 140,
+            color: '#009345' // rood
+            }, {
+            value: 150,
+            color: '#EC1C24' // oranje
+            }, {
+            color: '#EC1C24' // groen
+          }
+      ]},
+      ]
+    });
+  }
 }
