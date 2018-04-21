a1458dac2537aedba0a7f25ce7780cb96e54a66b
TimPijl <tim.pijl@finaps.nl>
2018-04-21 17:00:01 +0200

    Some styling

---
+++ src/pages/detailpage/detailpage.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+@Component({
+    selector: 'page-detail',
+    templateUrl: 'detailpage.html'
+})
+export class DetailPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
