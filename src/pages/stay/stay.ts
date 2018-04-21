7a7c7437ff4e2595c1af11f2a89e0b774cb5795f
TimPijl <tim.pijl@finaps.nl>
2018-04-21 20:27:48 +0200

    Added length of stay page

---
+++ src/pages/stay/stay.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+@Component({
+    selector: 'page-stay',
+    templateUrl: 'stay.html'
+})
+export class StayPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
